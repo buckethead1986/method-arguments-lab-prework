3559383fa42c62342145417d250776fc62e566c0
Robert Peck <rspeckpfs@gmail.com>
2017-08-12 22:39:23 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(nom_de_plume)
+  puts "Hi, my name is #{nom_de_plume}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
